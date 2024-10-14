4ddc5a4bec95351e63ac967d9f5644ae62cbe605
ZPU <93166306+xZPUHigh@users.noreply.github.com>
2024-10-14 07:12:49 +0700

    Create Gamelist.lua

---
+++ Gamelist.lua
@@ -0,0 +1,7 @@
+getgenv().Games = {
+    [2753915549] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- Blox Fruit Sea 1
+    [4442272183] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- Blox Fruit Sea 2
+    [7449423635] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- Blox Fruit Sea 3
+    [3351674303] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumBrandNewCar.lua", -- Driving Empire
+    [18901165922] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/c12281ed5e3842108c859ffbd9c1bb961.lua", -- Pet GO
+  }
