84165c6b62bc691813a0c280d3ead2351741b8dc
ZPU <93166306+xZPUHigh@users.noreply.github.com>
2024-11-28 21:46:36 +0700

    Delete Gamelist.lua

--- Gamelist.lua
+++
@@ -1,9 +0,0 @@
-getgenv().Games = {
-    [2753915549] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- B1
-    [4442272183] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- B2
-    [7449423635] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- B3
-    [3351674303] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/8972621456ab46418f51c66349616f22.lua", -- D
-    [18901165922] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/c12281ed5e3842108c859ffbd9c1bb961.lua", -- P
-    [16732694052] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/995ccfcb48bb4adaa66b371cfcc37ae4.lua", -- F
-
-  }
